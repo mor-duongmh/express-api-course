8e51e180e84db9f4032790e462a390c036d8fdf0
HaiDuong <115532203+haiduongmor@users.noreply.github.com>
2023-08-11 11:19:32 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import * as dotenv from 'dotenv'
 import app from './server'
 dotenv.config()
 app.listen(50002,()=>{
-    console.log('server on: http://localhost:50002')
+    console.log('server on http://localhost:50002')
 })
